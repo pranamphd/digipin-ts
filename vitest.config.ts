017470dbd633404b64ff034930337a5872685fbc
Pranam <pranam@pranam.phd>
2026-01-16 16:10:05 +0530

    chore(config): add ts, lint, test configurations

---
+++ vitest.config.ts
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2026 Pranam
+ * ORCID: https://orcid.org/0009-0007-9316-3616
+ *
+ * This code is licensed under the Apache License, Version 2.0.
+ *
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * QR Code:
+ * ![QR Code](/ORCID.png)
+ * Scan the QR code to access my ORCID profile.
+ */
+
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    environment: "node",
+    globals: true,
+    include: ["tests/**/*.test.ts"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html"],
+      exclude: ["dist/**", "**/*.config.ts", "eslint.config.ts"],
+    },
+  },
+});
